2933baed26071acc3019089fd8da075e4e2b54e6
Isaac Felipe <62969919+isaacfelipe1@users.noreply.github.com>
2024-07-26 16:12:22 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
-
-// Destrua a sessão para fazer logout
 session_destroy();
 
-// aqui faz com que  Desabilite o cache
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
 
-// Redirecione o usuário para a página de login
 header('Location: index.php');
 exit;
 ?>
