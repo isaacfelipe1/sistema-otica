4bf4f8375cd1f59add42da424448cab1ac21332e
Isaac Felipe <62969919+isaacfelipe1@users.noreply.github.com>
2024-07-26 16:09:56 -0300

    Update excluir_lentes.php

--- excluir_lentes.php
+++ excluir_lentes.php
@@ -4,14 +4,13 @@ $username = "oticasmundialit";
 $password = "Lindabrao875@678";
 $dbname = "oticasmundialita";
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Verifique a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-// Verifique se o ID do registro a ser excluído foi passado via POST
+
 if (isset($_POST['id'])) {
     $id_para_excluir = $_POST['id'];
-    // Execute a consulta de exclusão
+    
     $sql = "DELETE FROM lentes WHERE id = $id_para_excluir";
     
     if ($conn->query($sql) === TRUE) {
