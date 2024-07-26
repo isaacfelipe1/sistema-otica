288492ab03718f334d42bf37a8ad425c0d9ac03b
Isaac Felipe <62969919+isaacfelipe1@users.noreply.github.com>
2024-07-26 16:14:29 -0300

    Update visualizar_lentes.php

--- visualizar_lentes.php
+++ visualizar_lentes.php
@@ -2,23 +2,23 @@
 session_start();
 
 if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== true) {
-    header('Location:inicial.php'); // Redireciona para a página de login se o usuário não estiver autenticado
+    header('Location:inicial.php'); 
     exit;
 }
 ?>
 
 <?php
-// Configurações do banco de dados
+
 $servername = "mysql.oticasmundialita.com";
 $username = "oticasmundialit";
 $password = "Lindabrao875@678";
 $dbname = "oticasmundialita";
 
-// Conectar ao banco de dados
+
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Consulta SQL para recuperar os registros
+
 $sql = "SELECT * FROM lentes";
 $result = $conn->query($sql);
 
@@ -92,13 +92,13 @@ $result = $conn->query($sql);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
         function retornar() {
-            // Redireciona para a página de cadastro
+           
             window.location.href = "inicial.php"; 
         }
     </script>
 </body>
 </html>
 <?php
-// Fechar a conexão com o banco de dados
+
 $conn = null;
 ?>
