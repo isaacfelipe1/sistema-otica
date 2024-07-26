999428a6132f6f45b694e1a3f3dcb33e76e91ded
Isaac Felipe <62969919+isaacfelipe1@users.noreply.github.com>
2024-07-26 16:12:58 -0300

    Update processar_cadastro.php

--- processar_cadastro.php
+++ processar_cadastro.php
@@ -1,19 +1,15 @@
 <?php
-// Conexão com o banco de dados (substitua pelas suas informações)
 $servername = "xmysql.oticasmundialita.com";
 $username = "oticasmundialit";
 $password = "Lindabrao875@678";
 $dbname = "oticasmundialita";
 
-// Conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Conexão com o banco de dados falhou: " . $conn->connect_error);
 }
 
-// Processar os dados do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tipo_lentes = $_POST["tipo_lentes"];
     $fornecedor = $_POST["fornecedor"];
@@ -21,10 +17,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $acessorio1_nome = $_POST["acessorio1_nome"];
     $acessorio1_cor = $_POST["acessorio1_cor"];
 
-    // Verifica se a variável "tratamentos" está definida no POST
     $tratamentos = isset($_POST["tratamentos"]) ? implode(', ', $_POST["tratamentos"]) : "";
 
-    // Inserir os dados no banco de dados
     $sql = "INSERT INTO lentes (tipo_lentes, fornecedor, material, tratamentos, acessorio1_nome, acessorio1_cor)
             VALUES ('$tipo_lentes', '$fornecedor', '$material', '$tratamentos', '$acessorio1_nome', '$acessorio1_cor')";
 
@@ -35,7 +29,7 @@ if ($conn->query($sql) === TRUE) {
     echo "Erro ao cadastrar as lentes: " . $conn->error;
 }
 
-// Fechar a conexão com o banco de dados
+
 $conn->close();
 
 }
